cfeac9f3767fe75ac38e6492db10e364eff3a701
dpeix <dylanpeix@gmail.com>
2024-12-15 10:27:55 +0100

    Entity API

--- src/Entity/Message.php
+++ src/Entity/Message.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\MessageRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: MessageRepository::class)]
+#[ApiResource]
 class Message
 {
     #[ORM\Id]
